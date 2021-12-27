8dbf5e2e27ddc729ae1fb3b7d9365fcd4d5a0140
David Ukelere <david.ukelere291@gmail.com>
2021-12-27 07:51:44 +0100

    Completed profile section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
@@ -9,15 +9,45 @@ export default function Home() {
 
   return (
     <Wrapper>
-     <Map />
-     <ActionItems>Start</ActionItems>
+      <Map />
+      <ActionItems>
+        {/*Header  */}
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20215114.jpg" />
+          <Profile>
+            <Name>Paul Wilbur</Name>
+            <UserImage src='https://images.unsplash.com/photo-1529688530647-93a6e1916f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlnZXJpYW4lMjBtYW58ZW58MHx8MHx8&w=1000&q=80'/>
+          </Profile>
+        </Header>
+        {/* ActionButtons */}
+      </ActionItems>
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
-  flex flex-col h-screen bg-red-300
+  flex flex-col h-screen
 `
 const ActionItems = tw.div`
-  flex-1
+  flex-1 p-4
+`
+
+const Header = tw.div`
+  flex justify-between items-center
+`
+
+const UberLogo = tw.img`
+  h-28
+`
+
+const Profile = tw.div`
+  flex items-center
+`
+
+const Name = tw.div`
+  mr-4 w-20
+`
+
+const UserImage = tw.img`
+  h-12 w-12 rounded-full borderborder-gray-200 p-px
 `
